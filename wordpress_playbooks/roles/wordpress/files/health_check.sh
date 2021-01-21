ae288c8c6d3e8f2bf1f156e7ac0928d29f97f051
郭棉锐 <2659350920@qq.com>
2021-01-21 22:19:47 +0800

    update keepalived

--- wordpress_playbooks/roles/wordpress/files/health_check.sh
+++ wordpress_playbooks/roles/wordpress/files/health_check.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-URL=$1
-PORT=$2
+URL=$2
+PORT=$3
+
+echo $URL,$PORT
 
 curl -Is http://$URL:$PORT/info.php > /dev/null && echo "The remote side is healthy" || echo "The remote side is failed, please check"
